3fa7da76288277422626107e84fb44b85c572d6f
Maxime Lucas <maxime@mlcs.me>
2022-12-03 18:13:26 +0100

    🎉 Day 01

---
+++ day01/main.py
@@ -0,0 +1,36 @@
+from typing import TextIO
+
+def calc_calories_per_elf(file: TextIO) -> list[int]:
+  elves = []
+  count = 0
+
+  for line in file.readlines():
+    if line.strip() == '':
+      elves.append(count)
+      count = 0
+      continue
+
+    count += int(line.strip())
+
+  elves.append(count)
+  return elves
+
+def get_top_three_calories(elves: list[int]) -> int:
+  elves.sort(reverse=True)
+  return sum(elves[:3])
+
+print('### Advent of Code Day 1 ###\n')
+
+with open('example.txt') as file:
+  elves: list = calc_calories_per_elf(file)
+  print(f'Example for Part 1: {max(elves)}')
+
+  calories: int = get_top_three_calories(elves)
+  print(f'Example for Part 2: {calories}\n')
+
+with open('input.txt') as file:
+  elves: list = calc_calories_per_elf(file)
+  print(f'Answer for Part 1: {max(elves)}')
+
+  calories: int = get_top_three_calories(elves)
+  print(f'Answer for Part 2: {calories}')
