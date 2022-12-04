06e3dcfd29fcd06ae1f29d0830880d0166d7b8ed
Maxime Lucas <maxime@mlcs.me>
2022-12-04 13:40:57 +0100

    🎉 Day 03

---
+++ day03/main.py
@@ -0,0 +1,48 @@
+from typing import TextIO
+
+def get_rucksacks(file: TextIO) -> list[list]:
+  return [list(line.strip()) for line in file.readlines()]
+
+def get_priorities(item: str) -> int:
+  return ord(item) - (ord('`') if item.islower() else ord('&'))
+
+def calc_priorities_part1(rucksacks: list[list]) -> int:
+  priorities: int = 0
+
+  for rucksack in rucksacks:
+    first_compartment = rucksack[:len(rucksack)//2]
+    second_compartment = rucksack[len(rucksack)//2:]
+    common_items = list(set(first_compartment) & set(second_compartment))
+
+    for item in common_items:
+      priorities += get_priorities(item)
+
+  return priorities
+
+def calc_priorities_part2(rucksacks: list[list]) -> int:
+  priorities: int = 0
+  groups = [rucksacks[idx:idx + 3] for idx in range(0, len(rucksacks), 3)]
+
+  for group in groups:
+    badge = list(set(group[0]) & set(group[1]) & set(group[2]))[0]
+    priorities += get_priorities(badge)
+
+  return priorities
+
+print('### Advent of Code Day 3 ###\n')
+
+with open('example.txt') as file:
+  rucksacks = get_rucksacks(file)
+  priorities = calc_priorities_part1(rucksacks)
+  print(f'Example for Part 1: {priorities}')
+
+  priorities = calc_priorities_part2(rucksacks)
+  print(f'Example for Part 2: {priorities}\n')
+
+with open('input.txt') as file:
+  rucksacks = get_rucksacks(file)
+  priorities = calc_priorities_part1(rucksacks)
+  print(f'Answer for Part 1: {priorities}')
+
+  priorities = calc_priorities_part2(rucksacks)
+  print(f'Answer for Part 2: {priorities}\n')
