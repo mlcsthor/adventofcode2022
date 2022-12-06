ef1575a5db8ca085db815fb87d8a3519996c6642
Maxime Lucas <maxime@mlcs.me>
2022-12-06 15:20:21 +0100

    🎉 Day 06

---
+++ day06/main.py
@@ -0,0 +1,25 @@
+def find_first_marker(buffer: str, sequence_size: int):
+  i = 0
+  
+  while len(set(buffer[i:i + sequence_size])) != sequence_size:
+    i += 1
+
+  return i + sequence_size
+
+print('### Advent of Code Day 4 ###\n')
+
+with open('example.txt') as file:
+  lines = file.readlines()
+  marker = find_first_marker(lines[0].strip(), sequence_size=4)
+  print(f'Example for Part 1: {marker}')
+
+  marker = find_first_marker(lines[0].strip(), sequence_size=14)
+  print(f'Example for Part 2: {marker}\n')
+
+with open('input.txt') as file:
+  lines = file.readlines()
+  marker = find_first_marker(lines[0].strip(), sequence_size=4)
+  print(f'Answer for Part 1: {marker}')
+
+  marker = find_first_marker(lines[0].strip(), sequence_size=14)
+  print(f'Answer for Part 1: {marker}')
